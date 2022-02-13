47863d0493fdd336820b9de55172bf29e68a9430
Daniel Mendez <soliddan211@gmail.com>
2022-02-12 19:21:25 -0500

    added routes for api and html

---
+++ routes/apiRoutes.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
+  });
+
+  app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    newNote.id = uuidv4();
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      let notes = JSON.parse(data);
+      notes.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        res.json(notes);
+      });
+    });
+  });
+
+  app.delete("/api/notes", (req, res) => {
+    const id = req.params.id;
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      let newNotesArr = notes.filter((note) => {
+        return id !== note.id;
+      });
+      fs.writeFile("./db/db.json", JSON.stringify(newNotesArr), (err) => {
+        if (err) throw err;
+        res.json(newNotesArr);
+      });
+    });
+  });
+};
